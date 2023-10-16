a80849e8e4aa156babf37c7ecab0cfb30ff69fe1
Сергей <Sergy87@yandex.ru>
2023-10-16 20:33:42 +0300

    hw5

--- hw5/Catalog.java
+++ hw5/Catalog.java
@@ -1,3 +1,46 @@
 public class Catalog {
+    
+    private int id;
+    private String name;
+    private int price;
+    private int count;
 
+    public Catalog(int id, String name, int price, int count) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
--- hw5/Main.java
+++ hw5/Main.java
@@ -1,3 +1,8 @@
-public static void main(String[] args){
-    
+public class Main {
+    public static void main(String[] args) {
+
+        Payment payment = new Payment();
+        Search search = new Search();
+        Storage srorage = new Storage();
+    }
 }
